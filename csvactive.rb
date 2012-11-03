0fd104d87159a7d8982fe5a9ce099e67a06319fb
John Gesimondo <john@jmondo.com>
2012-11-02 18:33:46 -0700

    put stuff in classes

--- csvactive.rb
+++ csvactive.rb
@@ -13,62 +13,67 @@ ActiveRecord::Base.establish_connection(dbconfig)
 
 # integers, currency, percents (truncate)
 # TODO: phone numbers!
-FLOAT_REGEX = /^(?<neg>[\(-]?)\$?(?<val>\.?\d[\d,\.]*)(?<perc>%?)\)?\s*$/
-DATA_TYPES = {
-  "String" => :text,
-  "Float" => :float,
-  "Time" => :datetime
-}
-
-file_path = ARGV[0]
-raise 'you need to pass a csv to import' unless file_path
-
-number_converter =
-  proc do |field|
-    match = FLOAT_REGEX.match(field)
-    if match
-      field = match[:val].to_f
-      field *= -1 if !match[:neg].empty?
-      field /= 100 if !match[:perc].empty?
+class CSVParser
+  FLOAT_REGEX = /^(?<neg>[\(-]?)\$?(?<val>\.?\d[\d,\.]*)(?<perc>%?)\)?\s*$/
+  NUMBER_CONVERTER =
+    proc do |field|
+      match = FLOAT_REGEX.match(field)
+      if match
+        field = match[:val].to_f
+        field *= -1 if !match[:neg].empty?
+        field /= 100 if !match[:perc].empty?
+      end
+      field
     end
-    field
+
+  TIME_CONVERTER =
+    proc do |field|
+      Chronic.parse(field) || field
+    end
+
+  CSV_OPTIONS = {
+    converters: [NUMBER_CONVERTER, TIME_CONVERTER],
+    headers: :first_row,
+    return_headers: false,
+    encoding: 'UTF-8'
+  }
+
+  def initialize(file_path)
+    @file_path = file_path
   end
 
-time_converter =
-  proc do |field|
-    Chronic.parse(field) || field
+  def csv
+    @csv ||= CSV.open(@file_path, 'rb', CSV_OPTIONS)
   end
 
-csv_options = {
-  converters: [number_converter, time_converter],
-  headers: :first_row,
-  return_headers: false,
-  encoding: 'UTF-8'
-}
-
-@@csv_array = CSV.open(file_path, 'rb', csv_options).to_a
-@@columns_types = @@csv_array.first.to_hash
-@@columns_types.each do |header, value|
-  @@columns_types[header] = value.class.to_s
+  def column_types
+    unless @column_types
+      @columns_types = csv.shift.to_hash
+      @columns_types.each do |header, value|
+        @columns_types[header] = value.class.to_s
+      end
+    end
+  end
 end
 
-%x( rm ./data.sqlite3 )
-
 class CreateThings < ActiveRecord::Migration
+  DATA_TYPES = {
+    "String" => :text,
+    "Float" => :float,
+    "Time" => :datetime
+  }
   def up
     create_table :things do |t|
-      @@columns_types.each do |title, type|
+      @@csv_parser.column_types.each do |title, type|
         t.column title.gsub(' ','_').downcase, DATA_TYPES[type]
       end
     end
   end
 end
 
-CreateThings.migrate(:up)
-
 class Thing < ActiveRecord::Base
   def self.import_data
-    @@csv_array.each do |row|
+    @@csv_parser.csv.each do |row|
       attributes = {}
       row.to_hash.each do |header, value|
         attributes[header.gsub(' ','_').downcase] = value
@@ -78,6 +83,15 @@ class Thing < ActiveRecord::Base
   end
 end
 
+file_path = ARGV[0]
+raise 'you need to pass a csv to import' unless file_path
+
+%x( rm ./data.sqlite3 )
+@@csv_parser = CSVParser.new(file_path)
+
+CreateThings.migrate(:up)
 Thing.import_data
 
 binding.pry
+
+# exit to finish
